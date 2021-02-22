8425f4c27a4fb15b50be67254496c41fe7ee4d7f
10-Is-Mana <10suism.ashida@gmail.com>
2021-02-22 14:25:07 +0900

    bug fix

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 var tabs = document.getElementsByClassName('tab');
 var hash = window.location.hash.substr(1);
 
-if(hash){
-    document.getElementsByClassName(hash).classList.add('active');
-}
-
 function clickTab(e){
     for (j = 0; j < tabs.length; j++){
         tabs[j].classList.remove('active')
@@ -14,4 +10,8 @@ function clickTab(e){
 
 for (i = 0; i < tabs.length; i++){
     tabs[i].addEventListener('click', clickTab)
+}
+
+if(hash){
+    document.getElementsByClassName(hash).classList.add('active');
 }
