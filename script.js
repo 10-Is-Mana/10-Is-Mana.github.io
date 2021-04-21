d4cbaf78c5b6b3319d80709b214a0b15671f12be
10-Is-Mana <10suism.ashida@gmail.com>
2021-04-21 23:54:15 +0900

    updated

--- script.js
+++ script.js
@@ -15,3 +15,17 @@ for (i = 0; i < tabs.length; i++){
 if(hash){
     document.getElementsByClassName(hash)[0].classList.add('active');
 }
+
+function expandCard(pageID){
+    Array.from(document.getElementsByClassName("card")).forEach(element => element.setAttribute("class", "card-hidden"));
+    var card = document.createElement("div");
+    card.setAttribute("class", "expanded-card");
+    card.setAttribute("id", "expanded-card");
+    Array.from(document.getElementById(pageID).childNodes).forEach(element => card.appendChild(element.cloneNode(true)));
+    card.lastElementChild.firstChild.innerHTML = "Back";
+    card.lastElementChild.firstChild.setAttribute("onclick", "(function() { \
+        document.getElementById('expanded-card').remove(); \
+        Array.from(document.getElementsByClassName('card-hidden')).forEach(element => element.setAttribute('class', 'card')); \
+      })()");
+    document.getElementById("contents").appendChild(card);
+}
